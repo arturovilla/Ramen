e458e74af32b1f2af6c67bdbfc7ec1d046381f09
Arturo Villalobos <rturohouse@gmail.com>
2023-12-03 17:55:41 -0600

    editing navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,6 @@ import {
 	AiOutlineMenu,
 	AiOutlineClose,
 	AiOutlineLinkedin,
-	AiOutlineGithub,
 	AiOutlineMail,
 } from "react-icons/ai";
 
@@ -63,18 +62,18 @@ function Navbar() {
 
 						<Link href="/work">
 							<li className="ml-10 hover:border-b hover:border-gruvpink text-lg py-1">
-								Work
+								Menu
 							</li>
 						</Link>
 
 						<Link href="/resume">
 							<li className="ml-10 hover:border-b hover:border-gruvpink text-lg py-1">
-								Resume
+								Locations
 							</li>
 						</Link>
-						<Link href="/contact">
+						<Link href="https://order.toasttab.com/online/bakudan-ramen-stone-oak">
 							<li className="ml-10 text-lg py-1 px-5 text-gruv rounded-md border border-gruvpink hover:bg-gruvpink hover:text-[#0C0D1C]  transition duration-150 ease-in-out">
-								Contact
+								Order Now
 							</li>
 						</Link>
 					</ul>
@@ -85,7 +84,7 @@ function Navbar() {
 						onClick={handleNav}
 						className="sm:hidden pl-20 cursor-pointer "
 					>
-						<AiOutlineMenu size={25} color="#E95584" />
+						<AiOutlineMenu size={25} color="#FFF" />
 					</div>
 				</div>
 			</div>
@@ -93,14 +92,14 @@ function Navbar() {
 			<div
 				className={
 					menuOpen
-						? "fixed left-0 top-0 w-[100%] sm:hidden h-screen bg-gray-900/70 backdrop-blur-sm p-10 ease-in duration-500"
+						? "fixed left-0 top-0 w-[100%] sm:hidden h-screen bg-black/70 backdrop-blur-sm p-10 ease-in duration-500"
 						: "fixed left-[-100%] top-0 p-10 ease-in duration-500"
 				}
 			>
 				{/* inside mobile menu */}
 				<div className="flex w-full items-center justify-end ">
 					<div onClick={handleNav} className="cursor-pointer">
-						<AiOutlineClose size={20} color="#E95584" />
+						<AiOutlineClose size={20} color="#FFF" />
 					</div>
 				</div>
 				<div className="flex-col items-center text-center text-5xl py-4 text-gruvred">
@@ -118,7 +117,7 @@ function Navbar() {
 								onClick={() => setMenuOpen(false)}
 								className="hover:border-b border-gruvgreen pt-10 pb-3"
 							>
-								Work
+								Menu
 							</li>
 						</Link>
 
@@ -127,7 +126,7 @@ function Navbar() {
 								onClick={() => setMenuOpen(false)}
 								className="hover:border-b border-gruvgreen pt-10 pb-3"
 							>
-								Resume
+								Locations
 							</li>
 						</Link>
 						<Link href="/contact">
@@ -135,7 +134,7 @@ function Navbar() {
 								onClick={() => setMenuOpen(false)}
 								className="hover:border-b border-gruvgreen pt-10 pb-3"
 							>
-								Contact
+								Order Now
 							</li>
 						</Link>
 					</ul>
@@ -143,27 +142,20 @@ function Navbar() {
 				{/* socials, legal, and other links inside the mobile menu */}
 				<div className="flex flex-row justify-around pt-52 items-center">
 					<a
-						href="https://www.linkedin.com/in/rtvro/"
+						href="https://www.linkedin.com/company/bakudan-ramen/about/"
 						target="_blank"
 					>
 						<AiOutlineLinkedin
 							size={40}
 							className="cursor-pointer"
-							color="#F12F26"
-						/>
-					</a>
-					<a href="https://github.com/arturovilla" target="_blank">
-						<AiOutlineGithub
-							size={40}
-							className="cursor-pointer"
-							color="#F12F26"
+							color="#A3A3A3"
 						/>
 					</a>
 					<a href={`mailto:${email}`}>
 						<AiOutlineMail
 							size={40}
 							className="cursor-pointer"
-							color="#F12F26"
+							color="#A3A3A3"
 						/>
 					</a>
 				</div>
