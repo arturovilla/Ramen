f7dedfa3cfd5dcd19ef0823077ff9f1e61edf5b4
Arturo Villalobos <rturohouse@gmail.com>
2023-12-04 00:18:51 -0600

    fixed font issue

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,17 @@ import { Roboto_Flex } from 'next/font/google'
 const lithos = localFont({
 	src: "..//public/fonts/Lithos-Bold.ttf",
 	variable: "--font-lithos-bold",
+  display: 'swap'
 });
 const robotoFlex = Roboto_Flex({
   subsets: ['latin'],
   variable: '--font-roboto-flex',
+  display: 'swap'
 });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${lithos.variable},${robotoFlex.variable}`}>
+    <main className={`${lithos.variable} ${robotoFlex.variable}`}>
       <div className='pt-5'>
         <Navbar />
       </div>
