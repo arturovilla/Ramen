d51f4fabc2ddafd6423a6d9fe83991bae024d525
Arturo Villalobos <rturohouse@gmail.com>
2023-12-11 00:02:42 -0600

    added more drink items

--- components/Menucard.js
+++ components/Menucard.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Menuitem from './Menuitem'
 import cocktails from "../public/cocktails.json"
+import wines from "../public/wines.json"
+import beer_draft from "../public/beer_draft.json"
+import beer_cans from "../public/beer_can.json"
+
 
 function Menu(props) {
   const menuItems = props.data.map((item, index) => (
-    <Menuitem key={index} item={item} />
+    <Menuitem key={index} item={item} style={props.style}/>
   ));
 
   return menuItems
@@ -18,8 +22,43 @@ function Menucard() {
       <h1 className=' font-lithos text-6xl w-4/5 md:w-1/2 text-center border-b pb-2'>
         Menu
       </h1>
+      <h1 className=' mt-6 font-lithos text-3xl w-4/5 md:w-1/2 text-center  '>
+        Cocktails
+      </h1>
       <div className='w-9/12 justify-center mt-5 grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-5'>
-        <Menu data={cocktails} />
+        <Menu data={cocktails} style="border-b"/>
+      </div>
+      <h1 className=' mt-6 font-lithos text-3xl w-4/5 md:w-1/2 text-center  '>
+        Wine
+      </h1>
+      <div className='w-9/12 md:w-7/12 justify-center mt-5 grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-5'>
+        <Menu data={wines} style="border-b"/>
+      </div>
+      <h1 className=' mt-6 font-lithos text-3xl w-4/5 md:w-1/2 text-center '>
+        Beer
+      </h1>
+      <h1 className=' mt-1 font-lithos text-xl w-4/5 md:w-1/2 text-left border-b border-dashed'>
+        Draft
+      </h1>
+      <div className='w-6/12 justify-center mt-5 grid grid-cols-1 gap-1'>
+          <Menu  data={beer_draft} />
+      </div>
+      <h1 className=' mt-1 font-lithos text-xl w-4/5 md:w-1/2 text-left border-b border-dashed'>
+        Bottles/Cans
+      </h1>
+      <div className='w-6/12 justify-center mt-5 grid grid-cols-1 gap-1'>
+          <Menu  data={beer_cans} />
+      </div>
+      <h1 className='flex mt-1 font-lithos text-xl w-4/5 md:w-1/2 text-left border-b border-dashed justify-between'>
+        <p>
+          Beverages
+        </p>
+        <p>
+          2.50
+        </p>
+      </h1>
+      <div className='w-6/12 justify-center mt-5 grid grid-cols-1 gap-1'>
+        <p className='flex flex-col items-center text-red-500 w-auto px-3 py-2 ${props.style} transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-75'> Coke, Diet Coke, Orange Soda, Lemon-Lime, Lemonade, Dr. Pepper, Big Red</p>
       </div>
     </div>
   )
--- components/Menuitem.js
+++ components/Menuitem.js
@@ -3,9 +3,11 @@ import React from 'react'
 
 
 function Menuitem(props) {
+    const hasPriceGlass = props.item.Price_glass !== undefined;
+    const hasPriceBottle = props.item.Price_bottle !== undefined;
   
   return (
-    <div className=' flex flex-col items-center text-red-500 w-auto px-3 py-2 border-b transition ease-in-out  hover:-translate-y-1 hover:scale-105  duration-75'>
+    <div className={`flex flex-col items-center text-red-500 w-auto px-3 py-2 ${props.style} transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-75`}>
         <div className='flex justify-center space-x-4 text-[#d01d25] md:text-lg'>
             <h3 className=' font-roboto font-bold'>
                 {props.item.Name}
@@ -13,6 +15,20 @@ function Menuitem(props) {
             <p className=' font-roboto font-light italic'>
                 {props.item.Price}
             </p>
+
+            {hasPriceBottle && (<h3 className=' text-sm md:text-xl font-roboto font-bold'>
+                {props.item.Namewine}
+            </h3>)}
+            <div className='flex flex-col items-center'>
+                {hasPriceGlass && (<p className=' text-xs md:text-base flex flex-col items-center font-roboto font-light italic'>
+                    <p>Glass</p>
+                    {props.item.Price_glass}
+                </p>)}
+                {hasPriceBottle && (<p className=' text-xs md:text-base flex flex-col items-center font-roboto font-light italic'>
+                    <p>Bottle</p>
+                    {props.item.Price_bottle}
+                </p>)}
+            </div>
         </div>
         <div className=' flex flex-col items-center font-extralight text-xs md:text-lg'>
             <p>
