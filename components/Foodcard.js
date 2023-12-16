bc25b0338839702debb73aa61c5fc1f42dabbbfa
Arturo Villalobos <rturohouse@gmail.com>
2023-12-16 15:30:19 -0600

    menu changes

--- components/Foodcard.js
+++ components/Foodcard.js
@@ -15,6 +15,9 @@ function Foodcard() {
         <h1 className=' mt-6 font-lithos text-3xl w-4/5 md:w-1/2 text-center  '>
             Food Biotch
         </h1>
+        <div className='w-9/12 justify-center mt-5 grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-5'>
+            <Menu data={cocktails} style="border-b"/>
+        </div>
     </div>
   )
 }
